a193295d0e03430d85b8a3aaac8ebe28e485a89b
William Sebastian <okeefesebastian91@gmail.com>
2025-05-20 10:42:30 +0900

    make tool create action and tool params

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -6,4 +6,29 @@ class ToolsController < ApplicationController
   def new
     @tool = Tool.new
   end
+
+  def create
+    @user = User.find(params[:restaurant_id])
+    @tool = Tool.new(tool_params)
+    @tool.user = @user
+    if tool.save
+      redirect_to new_tool_path
+    else
+      render "tools/new", status: :unprocessable_entity
+    end
+  end
+
+  def tool_params
+    params.require(:tool).permit(:name, :description, :location, :condition, :rental_price, :category)
+  end
 end
+
+# belongs_to :user
+#   has_many :rentals
+
+#   validates :name, presence: true
+#   validates :description, presence: true
+#   validates :location, presence: true
+#   validates :condition, presence: true
+#   validates :rental_price, presence: true
+#   validates :category, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :tools, only: [:index, :new]
+  resources :tools, only: [:index, :new, :create]
 end
