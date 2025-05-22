16112a0c79a85ef096faca6c1beb9e3cc2c4b9f3
Ryan Ward <4577045+Ward-R@users.noreply.github.com>
2025-05-22 12:23:07 +0900

    added approve reject functionality to rentals and basic html

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  
+
   resources :tools, only: [:index, :new, :create, :show] do
     resources :rentals, only: [:create]
   end
+  resources :rentals, only: [:update]
   get 'users/dashboard', to: 'users#dashboard', as: :dashboard
 end
